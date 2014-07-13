d37239ad7b8120ead2bbbe56d82f8d6b3c461fbe
alex <yaobo-1986@163.com>
2014-07-13 21:43:58 +0800

    index

--- index.php
+++ index.php
@@ -169,7 +169,7 @@
 </div><!--header-->
 
 <div class="main clearfix">
-  <div class="max_width">
+  <div class="max_width clearfix">
     <div class="ad">
       <a href="#">
         <img src="./images/ad.jpg">
@@ -870,6 +870,60 @@
 
 
   </div><!--max_width-->
+</div>
+
+<div class="footer clearfix">
+  <div class="max_width clearfix">
+    <div class="all">
+      <h3>全站频道</h3>
+      <ul class="clearfix">
+        <?php for($i=0;$i<18;$i++){ ?>
+        <li><a href="#">品牌聚焦</a></li>
+        <?php } ?>
+      </ul>
+    </div>
+
+    <div class="fthree">
+      <dl>
+        <dt>我要订阅</dt>
+        <dd><a href="#">RSS订阅</a></dd>
+        <dd>RSS订阅</dd>
+        <dd>RSS订阅</dd>
+      </dl>
+      <dl>
+        <dt>我要订阅</dt>
+        <dd>RSS订阅</dd>
+        <dd>RSS订阅</dd>
+        <dd>RSS订阅</dd>
+      </dl>
+      <dl class="concern">
+        <dt>关注我们</dt>
+        <dd>
+          <a href="#">新浪微博</a>
+        </dd>
+        <dd class="qq">
+          <a href="#">新浪微博</a>
+        </dd>
+      </dl>
+    </div>
+
+    <div class="twocode">
+      <div class="imgw clearfix">
+        <img src="">
+      </div>
+      <span>ci086 官方微信</span>
+    </div>
+
+    <div class="fbtns">
+      <a class="btn " href="#">+ 我要投稿</a>
+      <a class="btn co">+ 广告合作</a>
+    </div>
+
+    <div class="footb">
+      Copyright&copy; 2012-2016  ci086.com  版权所有      湘ICP备13003363号
+    </div>
+
+  </div>
 </div>
 
 </body>
