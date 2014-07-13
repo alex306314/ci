07831b9ad6bf0be7da37b453674bbaaf47d6161d
alex <yaobo-1986@163.com>
2014-07-13 16:22:05 +0800

    add files

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title></title>
+  <link href="./css/common.css" rel="stylesheet" type="text/css">
+  <link href="./css/cistyle.css" rel="stylesheet" type="text/css">
+  <script type="text/javascript" src="./js/script.js"></script>
+</head>
+<body>
+<div class="header_top">
+  <div class="max_width">
+    <div class="l">
+      <a href="#">收藏ci086</a>
+      <a class="line"></a>
+      <a href="#">设为首页</a>
+    </div>
+    <div class="r">
+
+    </div>
+  </div><!--max-->
+</div>
+
+<div class="header">
+
+</div>
+
+</body>
+</html>
